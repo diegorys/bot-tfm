ac9778f6c6f3e8b9afc79203992c75c7d006a5cb
Diego de los Reyes <diegorys@gmail.com>
2022-04-28 18:39:31 +0200

    deleting old things

--- tools/dataset-generate.sh
+++
@@ -1,3 +0,0 @@
-aws lambda invoke --function-name tfm-$1-dataset-generate dataset.json
-cat dataset.json
-echo ""
--- tools/dataset-update.sh
+++
@@ -1,4 +0,0 @@
-# echo $payload
-aws lambda invoke --function-name tfm-$1-dataset-update --payload fileb://dataset.json response.json
-# cat output.json
-echo ""
