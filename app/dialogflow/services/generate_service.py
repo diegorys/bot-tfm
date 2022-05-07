3e417a1d8978212339d1e2bc3acf6736988a555c
Diego de los Reyes <diegorys@gmail.com>
2022-05-07 18:17:55 +0200

    Dialogflow

--- app/dialogflow/services/generate_service.py
+++ app/dialogflow/services/generate_service.py
@@ -5,17 +5,55 @@ import json
 class GenerateService:
     def execute(self, data, path: str):
         print("Generate")
-        self.generateEntities(path)
+        self.generateEntities(data, path)
         self.generateIntents(data, path)
         self.generateAgent(path)
         self.generatePackage(path)
 
-    def generateEntities(self, path: str):
+    def generateEntities(self, data, path: str):
         print("Generate entities")
         dir = f"{path}/entities"
-        print(dir)
         if not os.path.exists(dir):
             os.mkdir(dir)
+        finalEntities = {}
+        for item in data:
+            entities = item["entities"]
+            for entityName in entities.keys():
+                print(f"{entityName}: {entities[entityName]}")
+                if entityName not in finalEntities.keys():
+                    finalEntities[entityName] = []
+                if entities[entityName] not in finalEntities[entityName]:
+                    finalEntities[entityName].append(entities[entityName])
+        self.writeEntities(finalEntities, dir)
+
+    def writeEntities(self, entities, path):
+        for entityName in entities.keys():
+            output = self.generateEntityTemplate(entityName)
+            os.makedirs(os.path.dirname(f"{path}/{entityName}.json"), exist_ok=True)
+            with open(f"{path}/{entityName}.json", "w", encoding="utf-8") as f:
+                json.dump(output, f, ensure_ascii=False, indent=4)
+            output = self.generateValueTemplate(entities[entityName])
+            os.makedirs(os.path.dirname(f"{path}/{entityName}_entries_es.json"), exist_ok=True)
+            with open(f"{path}/{entityName}_entries_es.json", "w", encoding="utf-8") as f:
+                json.dump(output, f, ensure_ascii=False, indent=4)
+            output = self.generateValueTemplate(entities[entityName])
+
+    def generateEntityTemplate(self, entityName):
+        return {
+            "id": "xxx",
+            "name": entityName,
+            "isOverridable": True,
+            "isEnum": False,
+            "isRegexp": False,
+            "automatedExpansion": False,
+            "allowFuzzyExtraction": False,
+        }
+
+    def generateValueTemplate(self, values):
+        output = []
+        for value in values:
+            output.append({"value": value, "synonyms": []})
+        return output
 
     def generateIntents(self, data, path: str):
         print("Generate intents")
@@ -26,13 +64,12 @@ class GenerateService:
         for item in data:
             intent = item["intent"]
             if "" != intent:
-                print(intent)
+                # print(intent)
                 output = {"id": "", "name": intent}
                 with open(f"{path}/intents/{intent}.json", "w", encoding="utf-8") as f:
                     json.dump(output, f, ensure_ascii=False, indent=4)
                 with open(f"{path}/intents/{intent}_usersays_es.json", "w", encoding="utf-8") as f:
                     json.dump(output, f, ensure_ascii=False, indent=4)
-                    
 
     def generateAgent(self, path: str):
         print("Generate agent")
