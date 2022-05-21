3966d2c081c00c32dcdccc42b66482bc3346e620
Diego de los Reyes <diegorys@gmail.com>
2022-05-21 16:18:55 +0200

    Refactor

--- app/conversational_bot/domain/response.py
+++ app/conversational_bot/domain/response.py
@@ -2,22 +2,6 @@ from sso.domain.user import User
 
 
 class Response:
-    def __init__(self, user: User, text):
+    def __init__(self, user: User, text: str):
         self.user = user
         self.text = text
-        self.domain = ""
-        self.intent = ""
-        self.probability = 0
-        self.command = ""
-
-    def setDomain(self, domain):
-        self.domain = domain
-
-    def setIntent(self, intent):
-        self.intent = intent
-
-    def setProbability(self, probability):
-        self.probability = probability
-
-    def setCommand(self, command):
-        self.command = command
