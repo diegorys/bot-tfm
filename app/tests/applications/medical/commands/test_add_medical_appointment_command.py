21be86c5fb8a6797b39609eda66b65617647133b
Diego de los Reyes <diegorys@gmail.com>
2022-06-04 16:02:59 +0200

    tests

--- app/tests/applications/medical/commands/test_add_medical_appointment_command.py
+++ app/tests/applications/medical/commands/test_add_medical_appointment_command.py
@@ -1,15 +1,17 @@
+import os
 from src.applications.medical.domain.medical_appointment import MedicalAppointment
 from tests.applications.medical.mothers.add_medical_appointment_command_mother import AddMedicalAppointmentCommandMother
 from tests.sso.mothers.user_mother import UserMother
 
+os.environ["STAGE"] = "test"
 
 def test_execute():
     command = AddMedicalAppointmentCommandMother.getValid()
     user = UserMother.getValid()
-    args = {"cita": "especialidadtest", "date-time": "18:00"}
+    args = {"cita": "especialidadtest", "cuando": "18:00"}
 
     appointment: MedicalAppointment = command.execute(user, args)
 
     assert appointment.user.username == user.username
     assert appointment.speciality.name == args["cita"]
-    assert appointment.date.date == args["date-time"]
+    assert appointment.date.date == args["cuando"]
--- app/tests/applications/medical/commands/test_add_medication_user_command.py
+++ app/tests/applications/medical/commands/test_add_medication_user_command.py
@@ -1,15 +1,20 @@
+import os
 from src.applications.medical.domain.medication_user import MedicationUser
-from tests.applications.medical.mothers.add_medication_user_command_mother import AddMedicationUserCommandMother
+from tests.applications.medical.mothers.add_medication_user_command_mother import (
+    AddMedicationUserCommandMother,
+)
 from tests.sso.mothers.user_mother import UserMother
+
+os.environ["STAGE"] = "test"
 
 
 def test_execute():
     command = AddMedicationUserCommandMother.getValid()
     user = UserMother.getValid()
-    args = {"medicamento": "medicationtest", "date-time": "18:00"}
+    args = {"medicamento": "medicationtest", "cuando": "18:00"}
 
     medicationUser: MedicationUser = command.execute(user, args)
 
     assert medicationUser.user.username == user.username
     assert medicationUser.medication.name == args["medicamento"]
-    assert medicationUser.date.date == args["date-time"]
+    assert medicationUser.date.date == args["cuando"]
